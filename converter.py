72ebb33e864c8b21ff203fd15d3ccdad30ea76d8
Jose Seminario <chuquipoma123@gmail.com>
2026-07-16 17:17:13 -0500

    Integrate exchange rate API

--- converter.py
+++ converter.py
@@ -1,16 +1,38 @@
+import requests
+
 def get_exchange_rate(selected_option):
 
     if selected_option == "1":
-        return 3.40
+        base_currency = "USD"
+        target_currency = "PEN"
 
     elif selected_option == "2":
-        return 3.95
+        base_currency = "EUR"
+        target_currency = "PEN"
 
     elif selected_option == "3":
-        return 0.92
+        base_currency = "USD"
+        target_currency = "EUR"
 
     elif selected_option == "4":
-        return 4.60
+        base_currency = "GBP"
+        target_currency = "PEN"
+
+    url = f"https://open.er-api.com/v6/latest/{base_currency}"
+
+    try:
+
+        response = requests.get(url)
+
+        exchange_data = response.json()
+
+        exchange_rate = exchange_data["rates"][target_currency]
+
+        return exchange_rate
+
+    except:
+
+        return None
 
 def convert_currency(selected_direction, amount, exchange_rate):
     if selected_direction == "1":
